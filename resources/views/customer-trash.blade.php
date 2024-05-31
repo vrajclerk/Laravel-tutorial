6c6dcc2e97a6a9888dd381652b56620f58635c0b
vrajclerk <vrajclerk04@gmail.com>
2024-05-31 15:46:33 +0530

    session

--- resources/views/customer-trash.blade.php
+++ resources/views/customer-trash.blade.php
@@ -10,7 +10,12 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Vraj Clerk</a>
+            <a class="navbar-brand" href="#"> 
+                @if(session()->has('customer_name'))
+                {{session()->get('customer_name')}}
+                @else
+                Guest
+                @endif</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
 use App\Models\Customer;
 use App\Http\Controllers\CustomerController;
+use Illuminate\Http\Request;
 /*
 |-------------------------------------------------------------------------
 |--------------------------------------------
@@ -22,13 +23,14 @@ use App\Http\Controllers\CustomerController;
 //     print_r($customers->toArray());
 //     // return view('customer',['customer'=>$customer]);
 // });
-
+    
 Route::get('/',function(){
     return view('index');
 });
 
 Route::get('/register',[RegistrationController::class,'index']);
 Route::post('/register',[RegistrationController::class,'register']);
+
 Route::get('/customer/create',[CustomerController::class,'create'])->name("customer.create");
 Route::post('/customer',[CustomerController::class,'store']);
 Route::get('/customer',[CustomerController::class,'view']);
@@ -38,3 +40,21 @@ Route::post('/customer/update/{id}',[CustomerController::class,'update'])->name(
 Route::get('/customer/trash',[CustomerController::class,'trash'])->name('customer.trash');
 Route::get('/customer/restore/{id}',[CustomerController::class,'restore'])->name('customer.restore');
 Route::get('/customer/force-delete/{id}',[CustomerController::class,'forceDelete'])->name('customer.force-delete');
+
+
+Route::get('/get-all-session',function(){
+    $session = session()->all();
+   p($session);
+});
+Route::get('set-session',function(Request $request){
+    $request->session()->put('customer_name','Vraj Clerk');
+    $request->session()->put('customer_id','12');
+    $request->session()->flash('status','success');
+    return redirect('get-all-session');
+
+});
+Route::get('destroy-session',function(Request $request){
+    $request->session()->forget('customer_name');
+    $request->session()->forget('customer_id');
+    return redirect('get-all-session');
+});
